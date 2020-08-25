d9a5efb96f3349f6133d7e59589cf1615d1ace98
abdullatifmouhamadi <abdullatif.mouhamadi@gmail.com>
2020-08-25 22:20:52 +0200

    updating db

---
+++ database/mysql.py
@@ -0,0 +1,25 @@
+
+from sh import mysqldump, cp, mysql#, docker
+
+
+class MYSQLConnection:
+    def __init__(self, db_host, db_user, db_password):
+        self.dbhost     = db_host
+        self.dbuser     = db_user
+        self.dbpassword = db_password
+
+
+    def execute(self, statement):
+        return mysql("-h", self.dbhost ,"-u",  self.dbuser, "-p" + self.dbpassword , "-e", "{}".format( statement ) )
+
+
+    def newuserdb(self, db_name, user_password):
+        self.execute(statement = "CREATE DATABASE IF NOT EXISTS {}".format(db_name))
+        self.execute(statement = "CREATE USER '{}'@'{}' IDENTIFIED BY '{}';".format(db_name, self.dbhost, user_password))
+        self.execute(statement = "GRANT ALL PRIVILEGES ON {}.* TO '{}'@'{}';".format(db_name, db_name, self.dbhost))
+        self.execute(statement = "FLUSH PRIVILEGES;")
+        return True
+
+
+
+
