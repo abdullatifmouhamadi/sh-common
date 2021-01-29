f77efeb5481458499dbbfdcbf270f6fa7529162c
abdullatifmouhamadi <abdullatif.mouhamadi@gmail.com>
2021-01-29 11:53:54 +0100

    update

--- test_lang.py
+++ test_lang.py
@@ -7,14 +7,17 @@ print("salut")
 
 #from pygoogletranslation import Translator
 from googletrans import Translator
+import polib
 
 
+
+"""
 
 translator = Translator()
 t = translator.translate("Salut comment tu vas. Bien et toi.", src='fr', dest='en')
 
 print(t.text)
-
+"""
 
 
 
