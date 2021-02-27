5d2907ff864a14fa8e2533e677a98dfbb8916d5c
abdullatifmouhamadi <abdullatif.mouhamadi@gmail.com>
2021-02-27 14:46:18 +0100

    disable barcode

--- odoo/controllers/product_import.py
+++ odoo/controllers/product_import.py
@@ -159,7 +159,7 @@ class ProductImport:
         else: # Le produit existe - On fait donc une simple mise à jour
             print("cas2 - Le produit existe - On fait donc une simple mise à jour")
             tmpProductTemplate = ProductTemplate(template[0])
-            self.api.write(productTemplate.model, [tmpProductTemplate.id], productTemplate.provide(['standard_price', 'taxex_id', 'default_code','standard_price', 'name', 'weight', 'list_price', 'categ_id', 'description_sale', 'image_1920']))
+            self.api.write(productTemplate.model, [tmpProductTemplate.id], productTemplate.provide(['barcode', 'standard_price', 'taxex_id', 'default_code','standard_price', 'name', 'weight', 'list_price', 'categ_id', 'description_sale', 'image_1920']))
         return None
 
     def __create_product_default(self, productTemplate, image_urls):
